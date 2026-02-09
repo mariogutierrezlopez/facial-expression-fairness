4ea5f69f964090e0b791a0b7d0f16427c480f13f
Mario Gutiérrez <marioguti033@gmail.com>
2026-02-09 13:21:29 +0100

    Implementacion main.py sesgos

--- src/main.py
+++ src/main.py
@@ -6,20 +6,32 @@ from src.data.datamodule import MultiPIEDataModule
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 
+# CONFIGURACION
+DATA_DIR = "/home12TB1/database/recognition/faces/MultiPie/data/"
+CSV_PATH = "/home12TB1/database/recognition/faces/MultiPie/demographic_info_cropped.csv"
+LEARNING_RATE = 2e-3
+BATCH_SIZE = 256
+NUM_WORKERS = 4
+MAX_EPOCHS = 20
+N_OUTPUTS = 6
 
-if __name__ == "__main__":
+#Factores de sesgo f
+BIAS_FACTORS = [0.0, 0.25, 0.5, 0.75, 1.0]
 
-    torch.set_float32_matmul_precision('high')
 
-    # LightningDataModule y LightningModule
+def run_experiment(exp_name, bias_type, bias_factor, target_class=None):
+
     data = MultiPIEDataModule(
-        data_dir="/home12TB1/database/recognition/faces/MultiPie/data/",
-        csv_path="/home12TB1/database/recognition/faces/MultiPie/demographic_info_cropped.csv",
-        batch_size=256,
-        num_workers=12
+        data_dir=DATA_DIR,
+        csv_path=CSV_PATH,
+        batch_size=BATCH_SIZE,
+        num_workers=12,
+        bias_type=bias_type,
+        bias_factor=bias_factor,
+        target_class=target_class
     )
 
-    model = ResNet50(n_outputs=6, lr=2e-3)
+    model = ResNet50(n_outputs=N_OUTPUTS, lr=LEARNING_RATE)
 
     logger = WandbLogger(log_model="all")
 
@@ -37,10 +49,38 @@ if __name__ == "__main__":
         accelerator="gpu",
         # devices=[0],
         precision="16-mixed",
-        max_epochs=15,
+        max_epochs=MAX_EPOCHS,
         log_every_n_steps=10,
         logger=logger,
         callbacks=[checkpoint_callback]
     )
 
     trainer.fit(model=model, datamodule=data)
+
+if __name__ == "__main__":
+
+    torch.set_float32_matmul_precision('high')
+
+
+    # SESGOS REPRESENTACIONALES
+    print("Analizando sesgos representacionales")
+
+    for f in BIAS_FACTORS:
+        run_experiment(
+            exp_name=f"Repres_bias_f{f}",
+            bias_type="representational",
+            bias_factor=f
+        )
+
+    # SESGOS ESTEREOTÍPICOS
+    # print("Analizando sesgos estereotípicos")
+
+    # TARGET_CLASS_ID = 2
+
+    # for f in BIAS_FACTORS:
+    #     run_experiment(
+    #         exp_name=f"Stereotipical_bias_f{f}",
+    #         bias_type="stereotipical",
+    #         bias_factor=f,
+    #         target_class=TARGET_CLASS_ID
+    #     )
